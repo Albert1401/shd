a75a321e9b270cf60a9520a5aa9c8266b74c2696
Albert1401 <Albert1401@mail.ru>
2015-10-05 20:49:34 +0300

    Odd files deleted

--- HW1/expression/Conjunction.java
+++
@@ -1,7 +0,0 @@
-package expression;
-
-public class Conjunction extends AbstractBinOperation {
-    public Conjunction(Expression operation1, Expression operation2){
-        super(operation1, operation2, '&');
-    }
-}
--- HW1/expression/Disjunction.java
+++
@@ -1,7 +0,0 @@
-package expression;
-
-public class Disjunction extends AbstractBinOperation {
-    public Disjunction(Expression operation1, Expression operation2){
-        super(operation1, operation2, '|');
-    }
-}
