3d09d0c0411b41496f87a2d37beb78314aa92ff4
Albert <Albert1401@mail.ru>
2017-03-14 20:11:17 +0300

    supa hot fix

--- HW3.java
+++ HW3.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 public class HW3 {
     public static void main(String[] args) throws IOException {
         Proofer proofer = new Proofer();
-        proofer.make(new ExpressionParser().parse(args[0]));
+        if (!proofer.make(new ExpressionParser().parse(args[0]))){
+            System.out.println("ложное выражение");
+        }
     }
 }
